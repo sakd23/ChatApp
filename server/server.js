99658a2d773a3b24741559b754a4ddb5d0f06ef0
sakd23 <sakshamdhasmana_ec20a15_81@dtu.ac.in>
2022-06-23 17:49:12 +0530

    make it better5656

--- server/server.js
+++ server/server.js
@@ -24,10 +24,6 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "public", "build", "index.html"))
   );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
 }
 
 
